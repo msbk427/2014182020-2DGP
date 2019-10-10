45345cd1f35d1a626d43f07929832118db4a4bb5
Home <msbk427@naver.com>
2019-10-11 00:25:31 +0900

    drill6 커밋7번째
    방향전환에 딜레이가 있음

--- Drills/Drill-06/drill_6_8_2.py
+++ Drills/Drill-06/drill_6_8_2.py
@@ -6,21 +6,21 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
 def drill_6_8_1(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
-    frame = 0
-    direction =1
+    global frame
+    global direction
     # draw p1-p2
     for i in range(0, 50, 2):
         t = i / 100
-        cx = (2 * (t+1) ** 2 - 3 * (t+1) + 1) * p1[0] +\
-             (-4 * (t+1) ** 2 + 4 * (t+1)) * p2[0] + (2 * (t+1) ** 2 - (t+1)) * p3[0]
+        cx = (2 * (t + 1) ** 2 - 3 * (t + 1) + 1) * p1[0] + \
+             (-4 * (t + 1) ** 2 + 4 * (t + 1)) * p2[0] + (2 * (t + 1) ** 2 - (t + 1)) * p3[0]
         x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
         y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
         clear_canvas()
-        if cx >= x:
-            direction = 0
-        elif cx < x:
+        if cx > x:
             direction = 1
-        kpu_ground.draw(KPU_WIDTH//2, KPU_HEIGHT//2)
+        elif cx < x:
+            direction = 0
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
         character.clip_draw(frame * 100, 100 * direction, 100, 100, x, y)
         frame = (frame + 1) % 8
         delay(0.05)
@@ -28,18 +28,18 @@ def drill_6_8_1(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
     # draw p2-p3
     for i in range(0, 100, 2):
         t = i / 100
-        cx = ((-(t+1) ** 3 + 2 * (t+1) ** 2 - t) * p1[0] + (3 * (t+1) ** 3 - 5 * (t+1) ** 2 + 2) * p2[0] + (
+        cx = ((-(t+1) ** 3 + 2 * (t+1) ** 2 - (t+1)) * p1[0] + (3 * (t+1) ** 3 - 5 * (t+1) ** 2 + 2) * p2[0] + (
                 -3 * (t+1) ** 3 + 4 * (t+1) ** 2 + (t+1)) * p3[0] + ((t+1) ** 3 - (t+1) ** 2) * p4[0]) / 2
         x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
         clear_canvas()
-        if cx >= x:
-            direction = 0
-        elif cx < x:
+        if cx > x:
             direction = 1
-        kpu_ground.draw(KPU_WIDTH//2, KPU_HEIGHT//2)
+        elif cx < x:
+            direction = 0
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
         character.clip_draw(frame * 100, 100 * direction, 100, 100, x, y)
         frame = (frame + 1) % 8
         delay(0.05)
@@ -47,18 +47,18 @@ def drill_6_8_1(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
     # draw p3-p4
     for i in range(0, 100, 2):
         t = i / 100
-        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - t) * p2[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p3[0] + (
-                -3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p4[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p5[0]) / 2
+        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - (t + 1)) * p2[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p3[
+            0] + (-3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p4[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p5[0]) / 2
         x = ((-t ** 3 + 2 * t ** 2 - t) * p2[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[0] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p5[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p5[1]) / 2
         clear_canvas()
-        if cx >= x:
-            direction = 0
-        elif cx < x:
+        if cx > x:
             direction = 1
-        kpu_ground.draw(KPU_WIDTH//2, KPU_HEIGHT//2)
+        elif cx < x:
+            direction = 0
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
         character.clip_draw(frame * 100, 100 * direction, 100, 100, x, y)
         frame = (frame + 1) % 8
         delay(0.05)
@@ -66,18 +66,18 @@ def drill_6_8_1(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
     # draw p4-p5
     for i in range(0, 100, 2):
         t = i / 100
-        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - t) * p3[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p4[0] + (
-                -3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p5[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p6[0]) / 2
+        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - (t + 1)) * p3[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p4[
+            0] + (-3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p5[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p6[0]) / 2
         x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p5[0] + (t ** 3 - t ** 2) * p6[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p5[1] + (t ** 3 - t ** 2) * p6[1]) / 2
         clear_canvas()
-        if cx >= x:
-            direction = 0
-        elif cx < x:
+        if cx > x:
             direction = 1
-        kpu_ground.draw(KPU_WIDTH//2, KPU_HEIGHT//2)
+        elif cx < x:
+            direction = 0
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
         character.clip_draw(frame * 100, 100 * direction, 100, 100, x, y)
         frame = (frame + 1) % 8
         delay(0.05)
@@ -85,18 +85,18 @@ def drill_6_8_1(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
     # draw p5-p6
     for i in range(0, 100, 2):
         t = i / 100
-        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - t) * p4[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p5[0] + (
-                -3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p6[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p7[0]) / 2
+        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - (t + 1)) * p4[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p5[
+            0] + (-3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p6[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p7[0]) / 2
         x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[0] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p6[0] + (t ** 3 - t ** 2) * p7[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p6[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[1] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p6[1] + (t ** 3 - t ** 2) * p7[1]) / 2
         clear_canvas()
-        if cx >= x:
-            direction = 0
-        elif cx < x:
+        if cx > x:
             direction = 1
-        kpu_ground.draw(KPU_WIDTH//2, KPU_HEIGHT//2)
+        elif cx < x:
+            direction = 0
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
         character.clip_draw(frame * 100, 100 * direction, 100, 100, x, y)
         frame = (frame + 1) % 8
         delay(0.05)
@@ -104,17 +104,17 @@ def drill_6_8_1(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
     # draw p6-p7
     for i in range(0, 100, 2):
         t = i / 100
-        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - t) * p5[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p6[0] + (
-                -3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p7[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p8[0]) / 2
+        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - (t + 1)) * p5[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p6[
+            0] + (-3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p7[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p8[0]) / 2
         x = ((-t ** 3 + 2 * t ** 2 - t) * p5[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p6[0] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p7[0] + (t ** 3 - t ** 2) * p8[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p5[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p6[1] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p7[1] + (t ** 3 - t ** 2) * p8[1]) / 2
         clear_canvas()
-        if cx >= x:
-            direction = 0
-        elif cx < x:
+        if cx > x:
             direction = 1
+        elif cx < x:
+            direction = 0
         kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
         character.clip_draw(frame * 100, 100 * direction, 100, 100, x, y)
         frame = (frame + 1) % 8
@@ -123,18 +123,18 @@ def drill_6_8_1(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
     # draw p7-p8
     for i in range(0, 100, 2):
         t = i / 100
-        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - t) * p6[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p7[0] + (
-                -3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p8[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p9[0]) / 2
+        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - (t + 1)) * p6[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p7[
+            0] + (-3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p8[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p9[0]) / 2
         x = ((-t ** 3 + 2 * t ** 2 - t) * p6[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p7[0] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p8[0] + (t ** 3 - t ** 2) * p9[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p6[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p7[1] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p8[1] + (t ** 3 - t ** 2) * p9[1]) / 2
         clear_canvas()
-        if cx >= x:
-            direction = 0
-        elif cx < x:
+        if cx > x:
             direction = 1
-        kpu_ground.draw(KPU_WIDTH//2, KPU_HEIGHT//2)
+        elif cx < x:
+            direction = 0
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
         character.clip_draw(frame * 100, 100 * direction, 100, 100, x, y)
         frame = (frame + 1) % 8
         delay(0.05)
@@ -142,17 +142,17 @@ def drill_6_8_1(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
     # draw p8-p9
     for i in range(0, 100, 2):
         t = i / 100
-        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - t) * p7[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p8[0] + (
-                -3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p9[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p10[0]) / 2
+        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - (t + 1)) * p7[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p8[
+            0] + (-3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p9[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p10[0]) / 2
         x = ((-t ** 3 + 2 * t ** 2 - t) * p7[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p8[0] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p9[0] + (t ** 3 - t ** 2) * p10[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p7[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p8[1] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p9[1] + (t ** 3 - t ** 2) * p10[1]) / 2
         clear_canvas()
-        if cx >= x:
-            direction = 0
-        elif cx < x:
+        if cx > x:
             direction = 1
+        elif cx < x:
+            direction = 0
         kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
         character.clip_draw(frame * 100, 100 * direction, 100, 100, x, y)
         frame = (frame + 1) % 8
@@ -161,17 +161,17 @@ def drill_6_8_1(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
     # draw p9-p10
     for i in range(0, 100, 2):
         t = i / 100
-        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - t) * p8[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p9[0] + (
-                -3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p10[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p1[0]) / 2
+        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - (t + 1)) * p8[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p9[
+            0] + (-3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p10[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p1[0]) / 2
         x = ((-t ** 3 + 2 * t ** 2 - t) * p8[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p9[0] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p10[0] + (t ** 3 - t ** 2) * p1[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p8[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p9[1] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p10[1] + (t ** 3 - t ** 2) * p1[1]) / 2
         clear_canvas()
-        if cx >= x:
-            direction = 0
-        elif cx < x:
+        if cx > x:
             direction = 1
+        elif cx < x:
+            direction = 0
         kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
         character.clip_draw(frame * 100, 100 * direction, 100, 100, x, y)
         frame = (frame + 1) % 8
@@ -180,18 +180,18 @@ def drill_6_8_1(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
     # draw p10-p1
     for i in range(0, 100, 2):
         t = i / 100
-        cx = (((-t + 1) ** 3 + 2 * (t + 1) ** 2 - t) * p9[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p10[0] + (
-                -3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p1[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p2[0]) / 2
+        cx = ((-(t + 1) ** 3 + 2 * (t + 1) ** 2 - (t + 1)) * p9[0] + (3 * (t + 1) ** 3 - 5 * (t + 1) ** 2 + 2) * p10[
+            0] + (-3 * (t + 1) ** 3 + 4 * (t + 1) ** 2 + (t + 1)) * p1[0] + ((t + 1) ** 3 - (t + 1) ** 2) * p2[0]) / 2
         x = ((-t ** 3 + 2 * t ** 2 - t) * p9[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p10[0] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p9[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p10[1] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
         clear_canvas()
-        if cx >= x:
-            direction = 0
-        elif cx < x:
+        if cx > x:
             direction = 1
-        kpu_ground.draw(KPU_WIDTH//2, KPU_HEIGHT//2)
+        elif cx < x:
+            direction = 0
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
         character.clip_draw(frame * 100, 100 * direction, 100, 100, x, y)
         frame = (frame + 1) % 8
         delay(0.05)
@@ -204,19 +204,17 @@ running = True
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 direction = 1
-size = 10
-points1 = [random.randint(0, 500), random.randint(0, 500)]
-points2 = [random.randint(0, 500), random.randint(0, 500)]
-points3 = [random.randint(0, 500), random.randint(0, 500)]
-points4 = [random.randint(0, 500), random.randint(0, 500)]
-points5 = [random.randint(0, 500), random.randint(0, 500)]
-points6 = [random.randint(0, 500), random.randint(0, 500)]
-points7 = [random.randint(0, 500), random.randint(0, 500)]
-points8 = [random.randint(0, 500), random.randint(0, 500)]
-points9 = [random.randint(0, 500), random.randint(0, 500)]
-points10 = [random.randint(0, 500), random.randint(0, 500)]
+frame = 0
+points1 = [random.randint(300, 800), random.randint(300, 800)]
+points2 = [random.randint(300, 800), random.randint(300, 800)]
+points3 = [random.randint(300, 800), random.randint(300, 800)]
+points4 = [random.randint(300, 800), random.randint(300, 800)]
+points5 = [random.randint(300, 800), random.randint(300, 800)]
+points6 = [random.randint(300, 800), random.randint(300, 800)]
+points7 = [random.randint(300, 800), random.randint(300, 800)]
+points8 = [random.randint(300, 800), random.randint(300, 800)]
+points9 = [random.randint(300, 800), random.randint(300, 800)]
+points10 = [random.randint(300, 800), random.randint(300, 800)]
 
-while(1):
+while 1:
     drill_6_8_1(points1, points2, points3, points4, points5, points6, points7, points8, points9, points10)
-
-
