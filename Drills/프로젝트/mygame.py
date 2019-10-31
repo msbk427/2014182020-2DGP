0afff3027eabe4874b80db7258d99ab39d97196a
Home <msbk427@naver.com>
2019-11-01 00:00:51 +0900

    2차발표
    https://youtu.be/uZgviXLfgJA

--- Drills/프로젝트/mygame.py
+++ Drills/프로젝트/mygame.py
@@ -6,6 +6,7 @@ import title_state
 title_x = 800
 title_y = 800
 
+
 pico2d.open_canvas(title_x, title_y)
 game_framework.run(title_state)
 pico2d.close_canvas()
--- Drills/프로젝트/title_state.py
+++ Drills/프로젝트/title_state.py
@@ -15,7 +15,7 @@ def enter():
 
 def exit():
     global image
-    del(image)
+    del image
     pass
 
 
